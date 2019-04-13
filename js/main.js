778ac264b48ccc57662b9912bc4a92a2d93f5dc4
Aleksandr Gerasimenko <avtoelectronic1987@gmail.com>
2019-04-13 15:23:08 +0300

    2 commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
- cureentDate = new Date.toUTCString()
+//  cureentDate = new Date.toUTCString()
  document.cookie =`You last visit = ${new Date}`
 document.body.appendChild(document.createElement('p')).innerText = document.cookie;
