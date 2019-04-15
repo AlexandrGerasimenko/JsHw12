9fe3f6d2ae9b2c4ea7509d9dcab89b11726c48cf
Aleksandr Gerasimenko <avtoelectronic1987@gmail.com>
2019-04-15 23:36:27 +0300

    2 commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-//  cureentDate = new Date.toUTCString()
- document.cookie =`You last visit = ${new Date}`
-document.body.appendChild(document.createElement('p')).innerText = document.cookie;
+
+// Захостить на гитхабе приложение, которое:
+
+// читает куки на клиенте, и если там есть дата последнего посещения, выводит ее на страницу
+// пишет в куки на клиенте текущую дату посещения
+
+let p = document.body.appendChild(document.createElement('p'));
+var res = document.cookie
+    .split ( "; " )
+    .map ( x =>  {
+        var tmp = x.split ( "=" );
+        tmp[0] === 'Your last visit' ? p.innerText =`${tmp[0]} + : + ${tmp[1]}  `: null;
+})
+ document.cookie =`Your last visit = ${new Date}`
